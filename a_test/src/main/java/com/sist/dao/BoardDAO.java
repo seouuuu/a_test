8d5ca1df61341c58aa8ef6fbbbc35a79a077fce7
seouuuu <ysa_1010@naver.com>
2021-12-24 11:46:35 +0900

    BoardDAO, GoodsDAO 완성

---
+++ a_test/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,13 @@
+package com.sist.dao;
+
+public class BoardDAO {
+	public int insert() {
+		System.out.println("등록하였습니다.");
+		return 1;
+	}
+	
+	public int delete() {
+		System.out.println("삭제하였습니다..");
+		return 1;
+	}
+}
---
+++ a_test/src/main/java/com/sist/dao/GoodsDAO.java
@@ -0,0 +1,7 @@
+package com.sist.dao;
+
+public class GoodsDAO {
+	public void insert() {
+		System.out.println("등록하였습니다.");
+	}
+}
